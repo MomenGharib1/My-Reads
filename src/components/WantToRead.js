24ad7df2d202f46a65b189d98723ebccc2db6c3d
Mo'men Gharib <94765709+MomenGharib1@users.noreply.github.com>
2022-08-14 23:10:32 +0200

    each shelf is a reusable component

---
+++ src/components/WantToRead.js
@@ -0,0 +1,40 @@
+import ShelfUpdater from "./ShelfUpdater";
+import PropTypes from "prop-types";
+
+const WantToRead = ({ Books, updateShelf, Shelf }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">Want to Read</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {Books.filter((b) => b?.shelf === Shelf).map((b) => (
+            <li key={b?.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${b?.imageLinks?.thumbnail})`,
+                    }}
+                  ></div>
+                  <ShelfUpdater book={b} updateShelf={updateShelf} />
+                </div>
+                <div className="book-title"> {b?.title}</div>
+                <div className="book-authors">{b?.authors}</div>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+WantToRead.propTypes = {
+  Books: PropTypes.array.isRequired,
+  updateShelf: PropTypes.func.isRequired,
+  Shelf: PropTypes.string.isRequired,
+};
+export default WantToRead;
